45394ea069192527c8061752af28b75564bf057e
Kaushik Borah <117542258+dngeonMaster1706@users.noreply.github.com>
2024-07-01 00:06:22 +0530

    Updated Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/kaushik_nits (2).pdf";
+import pdf from "../../Assets/../Assets/Kaushik_NITS_CV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
